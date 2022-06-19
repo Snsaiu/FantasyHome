a33d7026d30c6aa281fc4883d20099e2491b814a
Saiu <34592712+Snsaiu@users.noreply.github.com>
2022-06-19 22:56:27 +0800

    添加

--- FantasyHome.UI/Views/Components/WeatherComponent.xaml.cs
+++ FantasyHome.UI/Views/Components/WeatherComponent.xaml.cs
@@ -9,6 +9,64 @@ public partial class WeatherComponent : ContentView
 	{
 		InitializeComponent();
 	}
+
+    /// <summary>
+    /// token 值
+    /// </summary>
+    public static readonly BindableProperty TokenProperty = BindableProperty.Create(
+        "Token",
+        typeof(string),
+        typeof(WeatherComponent),
+        ""
+    );
+
+    public string Token
+    {
+        get { return (string)GetValue(TokenProperty); }
+        set { SetValue(TokenProperty, value); }
+    }
+
+    public static readonly BindableProperty AcIpProperty = BindableProperty.Create(
+        "AcIp",
+        typeof(string),
+        typeof(WeatherComponent),
+        ""
+    );
+
+    public string AcIp
+    {
+        get { return (string)GetValue(AcIpProperty); }
+        set { SetValue(AcIpProperty, value); }
+    }
+
+
+    public static readonly BindableProperty AcIdProperty = BindableProperty.Create(
+        "AcId",
+        typeof(string),
+        typeof(WeatherComponent),
+        ""
+    );
+
+    public string AcId
+    {
+        get { return (string)GetValue(AcIdProperty); }
+        set { SetValue(AcIdProperty, value); }
+    }
+
+    public static readonly BindableProperty AcK1Property = BindableProperty.Create(
+        "AcK1",
+        typeof(string),
+        typeof(WeatherComponent),
+        ""
+    );
+
+    public string AcK1
+    {
+        get { return (string)GetValue(AcK1Property); }
+        set { SetValue(AcK1Property, value); }
+    }
+
+
     #region ����
     public static readonly BindableProperty WeeksWeatherListProperty = BindableProperty.Create("WeeksWeatherList",
         typeof(IEnumerable<WeekWeatherListModel>), typeof(WeatherComponent));
@@ -54,7 +112,6 @@ public partial class WeatherComponent : ContentView
     }
 
 
-
     /// <summary>
     /// ������������
     /// </summary>
@@ -79,6 +136,7 @@ public partial class WeatherComponent : ContentView
         set { SetValue(CurrentRayProperty, value); }
     }
 
+    
 
     public static readonly BindableProperty TapCommandProperty = BindableProperty.Create("TapCommand",
         typeof(ICommand), typeof(WeatherComponent));
@@ -89,5 +147,10 @@ public partial class WeatherComponent : ContentView
         set { SetValue(TapCommandProperty, value); }
     }
     #endregion
+
+    private void Send()
+    {
+
+    }
 
 }
