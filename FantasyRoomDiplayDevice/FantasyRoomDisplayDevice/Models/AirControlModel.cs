6e2eef137cdeb949c6d71289ed40ee3ae0a7fb14
方留军 <snsaiu@outlook.com>
2022-07-08 18:22:07 +0800

    添加空调

---
+++ FantasyRoomDiplayDevice/FantasyRoomDisplayDevice/Models/AirControlModel.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+
+namespace FantasyRoomDisplayDevice.Models
+{
+    public class AirControlModel
+    {
+        public AirControlModel()
+        {
+            this.RunModes = new();
+            
+        }
+
+        public List<RunMode> RunModes { get; set; }
+        
+    }
+
+    /// <summary>
+    /// 运行模式
+    /// </summary>
+    public class RunMode
+    {
+        public RunMode()
+        {
+            this.State = false;
+            this.IconPath = "pack://application:,,,/Resources/Fonts/#aircontrol";
+        }
+        public string Name { get; set; }
+        public bool State { get; set; }
+        public string Icon { get; set; }
+        public string IconPath { get; set; }
+    }
+}
---
+++ FantasyRoomDiplayDevice/FantasyRoomDisplayDevice/Views/Components/ShowWindSpeedDialog.xaml.cs
@@ -0,0 +1,66 @@
+using System.Collections.Generic;
+using System.Windows;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+
+namespace FantasyRoomDisplayDevice.Views.Components
+{
+
+
+    public class WindSpeedItem
+    {
+        public WindSpeedItem(string content,bool ischecked)
+        {
+            this.Content = content;
+            this.IsChecked = ischecked;
+        }
+
+        public WindSpeedItem()
+        {
+            
+        }
+        public string Content { get; set; }
+        public bool IsChecked { get; set; }
+    }
+    
+    [ObservableObject]
+    public partial class ShowWindSpeedDialog : Window
+    {
+        public delegate void SelectedChangedDelegate(string content);
+
+        public event SelectedChangedDelegate SelectedChangedEvent;
+
+        [ICommand]
+        private void SelectedChange(object obj)
+        {
+            
+            string c = obj.ToString();
+            if (this.SelectedChangedEvent != null)
+                this.SelectedChangedEvent(c);
+        }
+
+        [ICommand]
+        private void CloseDialog()
+        {
+            this.Close();
+        }
+        public ShowWindSpeedDialog(string content)
+        {
+            List<WindSpeedItem> list = new();
+            list.Add(new WindSpeedItem("自动",content=="自动"?true:false));
+
+            
+            InitializeComponent();
+            this.DataContext = this;
+            for (int i = 0; i <= 100; i+=10)
+            {
+                bool c = content == i.ToString() ? true : false;
+                WindSpeedItem item = new WindSpeedItem(i.ToString(), c);
+                list.Add(item);
+            }
+            // list.Add(new WindSpeedItem("自动",false));
+
+            this.listview.ItemsSource = list;
+        }
+    }
+}
